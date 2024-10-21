996151ed529b873c0f8e32b19f952c4a0bf53775
Kacper Zawiszewski <60822138+BumBumT24@users.noreply.github.com>
2024-10-21 10:14:23 +0200

    Lab4-zad1

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
 const http = require('http');
-const path = require("path");
-const anymatch = require('anymatch');
-const v8 = require("v8");
-const hostname = '127.0.0.1';
-const port = 8888;
-const matchers = [ 'path/to/file.js', 'path/anyjs/**/*.js', /foo.js$/, string => string.includes('bar') && string.length > 10 ] ;
-const server = http.createServer((request, response) => {
- response.statusCode = 200;
- response.setHeader('Content-Type', 'text/plain');
- 
- response.end('Hello, from node.js serwer !\n');
+const fs = require('fs');
+const server = http.createServer(function (req, res) {
+    getTitles(res);
+}).listen(8000, "127.0.0.1");
 
-});
-server.listen(port, hostname, () => {
-    console.log(anymatch(matchers, 'path/to/file.js'));
-    console.log(anymatch(matchers, 'path/anyjs/baz.js'));
-    console.log(anymatch(matchers, 'path/to/foo.js'));
-    console.log(anymatch(matchers, 'path/to/bar.js'));
-    console.log(anymatch(matchers, 'bar.js'));
-    console.log("Anymatch is a Javascript module to match a string against a regular expression, glob, string or function that takies the string as an argument and returns a truthy or falsy value.");
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+function getTitles(res) {
+    fs.readFile('./titles.json', function (err, data) {
+        if (err) return hadError(err, res);
+        getTemplate(JSON.parse(data.toString()), res);  
+    });
+}
+
+function getTemplate(titles, res) {
+    fs.readFile('./template.html', function (err, data) {
+        if (err) return hadError(err, res);
+        formatHtml(titles, data.toString(), res);
+    });
+}
+
+function formatHtml(titles, tmpl, res) {
+    let html = tmpl.replace('%', titles.join('</li><li>'));
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(html);
+}
+
+function hadError(err, res) {
+    console.error(err);
+    res.end('Error');
+}
